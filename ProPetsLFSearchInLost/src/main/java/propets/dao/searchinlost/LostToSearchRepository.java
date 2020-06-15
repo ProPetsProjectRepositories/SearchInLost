1ab4cdbc96a5ffd694b2297bb1b55397839ec977
Vladimir <v_litva@mail.ru>
2020-06-15 11:57:20 +0300

    update 15.06.2020

--- ProPetsLFSearchInLost/src/main/java/propets/dao/searchinlost/LostToSearchRepository.java
+++ ProPetsLFSearchInLost/src/main/java/propets/dao/searchinlost/LostToSearchRepository.java
@@ -9,8 +9,8 @@ import propets.model.searchinlost.LostToSearch;
 
 public interface LostToSearchRepository extends ElasticsearchRepository<LostToSearch, String> {
 
-	@Query("{\'bool\':{\'must\':[{\'match\':{\'type\':\'?0\'}},{\'match\':{\'breed\':\'?1\'}},{\'match\':{\'sex\':\'?2\'}},{\'match\':{\'tags\':{\'query\':\'?6\',\'minimum_should_match\':\'?7%\'}}}],\'filter\':{\'geo_distance\':{\'distance\':'?5km',\'coordinates\':{\'lat\':\'?3\',\'lon\':\'?4\'}}}}}")
-	Set<LostToSearch> findPosts(String type, String breed, String sex, Double latitude, Double longitude, int radius,
+	@Query("{\"bool\":{\"must\":[{\"match\":{\"type\":\"?0\"}},{\"match\":{\"breed\":\"?1\"}},{\"match\":{\"sex\":\"?2\"}},{\"match\":{\"tags\":{\"query\":\"?6\",\"minimum_should_match\":\"?7%\"}}}],\"filter\":{\"geo_distance\":{\"distance\":\"?5km\",\"coordinates\":{\"lat\":\"?3\",\"lon\":\"?4\"}}}}}")
+	Set<LostToSearch> findPosts(String type, String breed, String sex, Double lat, Double lon, int radius,
 			String tags, int minPercent);
 
 }
